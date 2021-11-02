dfcbfe1fa31b3d88bdc5f9dd0727bbab0a192990
duong <tovanduonghp1996@gmail.com>
2021-11-01 21:34:41 -0700

    add redux-form

--- asm3_nâng cao/src/Component/menuComponent.js
+++ asm3_nâng cao/src/Component/menuComponent.js
@@ -1,3 +1,4 @@
+/* eslint-disable react/jsx-pascal-case */
 import React, { useState } from "react";
 import {
   CardImg,
@@ -6,13 +7,13 @@ import {
   BreadcrumbItem,
   Card,
   Button,
-  Form,
-  FormGroup,
   Label,
-  Input,
   Col,
+  Row,
 } from "reactstrap";
+import { Control, LocalForm, Errors } from "react-redux-form";
 import { Link } from "react-router-dom";
+
 function RenderMenu({ items }) {
   return (
     <Link
@@ -54,6 +55,7 @@ const Staff = function (props) {
 
   // ==============Add Staff=======================
   const [toggle, setToggle] = useState(false);
+
   const HandleAdd = () => {
     setToggle(!toggle);
   };
@@ -61,173 +63,12 @@ const Staff = function (props) {
     setToggle(!toggle);
   };
   // ===============validate===============
-  const [fulname, setFulname] = useState("");
-  const [doB, setDoB] = useState("");
-  const [startDate, setStartDate] = useState("");
-  const [department, setDepartment] = useState("");
-  const [salaryScale, setSalaryScale] = useState("");
-  const [annualLeave, setAnnualLeave] = useState("");
-  const [overTime, setOverTime] = useState("");
 
-  const [fulnameErr, setFulnameErr] = useState("");
-  const [doBErr, setDoBErr] = useState("");
-  const [startDateErr, setStartDateErr] = useState("");
-  const [departmentErr, setDepartmentErr] = useState("");
-  const handleChange = (e) => {
-    setFulname(e.target.value);
-    const fulnameErr = {};
-    if (e.target.value.length === 0) {
-      fulnameErr.fulnameShort = "Yêu cầu nhập";
-      return;
-    }
-    if (e.target.value.length < 5) {
-      fulnameErr.fulnameShort = "Họ và tên quá ngắn";
-    }
-    console.log(e.target.value);
-    console.log(e.target.value.length);
-    if (e.target.value.length > 15) {
-      fulnameErr.fulnameShort = "Họ và tên quá dài";
-    }
-    if (e.target.value.length >= 5) {
-    }
-    setFulnameErr(fulnameErr);
-  };
-  const handleBlur = () => {
-    const fulnameErr = {};
-    if (fulname.length < 5) {
-      fulnameErr.fulnameShort = "Họ và tên quá ngắn";
-    }
-    if (fulname.length === 0) {
-      fulnameErr.fulnameShort = "Yêu cầu nhập";
-    }
-    if (fulname.length > 15) {
-      fulnameErr.fulnameShort = "Họ và tên quá dài";
-    }
-
-    setFulnameErr(fulnameErr);
-  };
-  const handleChangeDoB = (e) => {
-    setDoB(e.target.value);
-    const doBErr = {};
-    if (doB.length !== 0) {
-      doBErr.doBError = "";
-    }
-    setDoBErr(doBErr);
-  };
-  const handleBlurDoB = () => {
-    const doBErr = {};
-    if (doB.length === 0) {
-      doBErr.doBError = "Yêu cầu nhập";
-    }
-    if (doB.length !== 0) {
-      doBErr.doBError = "";
-    }
-    setDoBErr(doBErr);
-  };
-
-  const handleChangeStartDate = (e) => {
-    setStartDate(e.target.value);
-    const startDateErr = {};
-    if (startDate.length !== 0) {
-      startDateErr.startDateError = "";
-    }
-    setStartDateErr(startDateErr);
-  };
-  const handleBlurStartDate = () => {
-    const startDateErr = {};
-    if (startDate.length === 0) {
-      startDateErr.startDateError = "Yêu cầu nhập";
-    }
-    if (startDate.length !== 0) {
-      startDateErr.startDateError = "";
-    }
-    setStartDateErr(startDateErr);
-  };
-
-  const handleChangeDepartment = (e) => {
-    setDepartment(e.target.value);
-    const departmentErr = {};
-    console.log(department.length);
-    if (departmentErr.length === 0) {
-      departmentErr.departmentError = "Yêu cầu chọn";
-    }
-    if (departmentErr.length !== 0) {
-      departmentErr.departmentError = "";
-    }
-    setDepartmentErr(departmentErr);
-  };
-  const handleBlurDepartment = () => {
-    const departmentErr = {};
-    if (departmentErr.length === 0) {
-      departmentErr.departmentError = "Yêu cầu chọn";
-    }
-    if (departmentErr.length !== 0) {
-      departmentErr.departmentError = "";
-    }
-    setDepartmentErr(departmentErr);
-  };
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    const isValid = formValidation();
-    if (isValid) {
-      const newStaff = {
-        id: props.items.length,
-        image: "/assets/images/alberto.png",
-        name: fulname,
-        doB: doB,
-        salaryScale: salaryScale,
-        department: department,
-        annualLeave: annualLeave,
-        overTime: overTime,
-      };
-      console.log(newStaff);
-      props.addStaff(newStaff);
-      alert("Đã thêm nhân viên");
-    }
-  };
-  const formValidation = () => {
-    const fulnameErr = {};
-    const doBErr = {};
-    const startDateErr = {};
-    const departmentErr = {};
-    let isValid = true;
-    if (fulname.length < 5) {
-      fulnameErr.fulnameShort = "Họ và tên quá ngắn";
-      isValid = false;
-    }
-    if (fulname.length === 0) {
-      fulnameErr.fulnameShort = "Yêu cầu nhập";
-      isValid = false;
-    }
-
-    if (fulname.length > 15) {
-      fulnameErr.fulnameLong = "Họ và tên quá dài";
-      isValid = false;
-    }
-    if (doB.length === 0) {
-      doBErr.doBError = "Yêu cầu nhập";
-      isValid = false;
-    }
-    if (startDate.length === 0) {
-      startDateErr.startDateError = "Yêu cầu nhập";
-      isValid = false;
-    }
-    console.log(department.length);
-    if (department.length === 0) {
-      departmentErr.departmentError = "Yêu cầu chọn";
-      isValid = false;
-    }
-    setFulnameErr(fulnameErr);
-    setDoBErr(doBErr);
-    setStartDateErr(startDateErr);
-    setDepartmentErr(departmentErr);
-    return isValid;
-  };
   // =====================Menu==========================
-  const Staff =
+  const Menu =
     props.items &&
     props.items.map((item) => <RenderMenu key={item.id} items={item} />);
+
   return (
     <div>
       <div className="container ">
@@ -295,85 +136,61 @@ const Staff = function (props) {
                 </p>
               </div>
               <hr />
-              <Form onSubmit={handleSubmit}>
-                <FormGroup row className="fomr-container">
+              <LocalForm className="fomr-container">
+                <Row>
                   <Label md={3} htmlFor="fulname">
                     Họ Tên
                   </Label>
                   <Col md={8}>
-                    <Input
-                      type="text"
+                    <Control.text
+                      className="form-group"
+                      model=".fullname"
                       id="fulname"
                       name="fulname"
                       placeholder="full Name"
-                      value={fulname}
-                      onChange={handleChange}
-                      onBlur={handleBlur}
                     />
-                    {Object.keys(fulnameErr).map((key, index) => {
-                      return (
-                        <div key={index} style={{ color: "red" }}>
-                          {fulnameErr[key]}
-                        </div>
-                      );
-                    })}
                   </Col>
-                </FormGroup>
-                <FormGroup row>
+                </Row>
+
+                <Row>
                   <Label md={3} htmlFor="doB">
                     Ngày sinh
                   </Label>
                   <Col md={8}>
-                    <Input
+                    <Control.text
                       type="date"
+                      model=".doB"
                       id="doB"
                       name="doB"
-                      value={doB}
-                      onChange={handleChangeDoB}
-                      onBlur={handleBlurDoB}
+                      className="form-group"
                     />
-                    {Object.keys(doBErr).map((key, index) => {
-                      return (
-                        <div key={index} style={{ color: "red" }}>
-                          {doBErr[key]}
-                        </div>
-                      );
-                    })}
                   </Col>
-                </FormGroup>
-                <FormGroup row>
+                </Row>
+
+                <Row>
                   <Label md={3} htmlFor="startDate">
                     Ngày vào công ty
                   </Label>
                   <Col md={8}>
-                    <Input
+                    <Control.text
                       type="date"
+                      className="form-group"
+                      model=".startDate"
                       id="startDate"
                       name="startDate"
-                      value={startDate}
-                      onChange={handleChangeStartDate}
-                      onBlur={handleBlurStartDate}
                     />
-                    {Object.keys(startDateErr).map((key, index) => {
-                      return (
-                        <div key={index} style={{ color: "red" }}>
-                          {startDateErr[key]}
-                        </div>
-                      );
-                    })}
                   </Col>
-                </FormGroup>
-                <FormGroup row>
+                </Row>
+
+                <Row>
                   <Label md={3} htmlFor="department">
                     Phòng ban
                   </Label>
                   <Col md={8}>
-                    <Input
-                      type="select"
+                    <Control.select
+                      model=".department"
                       name="department"
-                      value={department}
-                      onChange={handleChangeDepartment}
-                      onBlur={handleBlurDepartment}
+                      className="form-group"
                     >
                       <option>===Select===</option>
                       <option>Sale</option>
@@ -381,77 +198,73 @@ const Staff = function (props) {
                       <option>Marketing</option>
                       <option>IT</option>
                       <option>Finance</option>
-                    </Input>
-                    {Object.keys(departmentErr).map((key, index) => {
-                      return (
-                        <div key={index} style={{ color: "red" }}>
-                          {departmentErr[key]}
-                        </div>
-                      );
-                    })}
+                    </Control.select>
                   </Col>
-                </FormGroup>
-                <FormGroup row>
+                </Row>
+
+                <Row>
                   <Label md={3} htmlFor="salaryScale">
                     Hệ số lương
                   </Label>
                   <Col md={8}>
-                    <Input
+                    <Control.text
+                      className="form-group"
                       type="number"
+                      model=".salaryScale"
                       id="salaryScale"
                       name="salaryScale"
-                      value={salaryScale}
-                      onChange={(e) => {
-                        setSalaryScale(e.target.value);
-                      }}
                     />
                   </Col>
-                </FormGroup>
-                <FormGroup row>
+                </Row>
+
+                <Row>
                   <Label md={3} htmlFor="annualLeave">
                     Số ngày nghỉ còn lại
                   </Label>
                   <Col md={8}>
-                    <Input
+                    <Control.text
+                      className="form-group"
                       type="number"
+                      model=".annualLeave"
                       id="annualLeave"
                       name="annualLeave"
-                      value={annualLeave}
-                      onChange={(e) => {
-                        setAnnualLeave(e.target.value);
-                      }}
                     />
                   </Col>
-                </FormGroup>
-                <FormGroup row>
+                </Row>
+
+                <Row>
                   <Label md={3} htmlFor="overTime">
                     Số ngày đã làm thêm
                   </Label>
                   <Col md={8}>
-                    <Input
+                    <Control.text
+                      className="form-group"
                       type="number"
+                      model=".overTime"
                       id="overTime"
                       name="overTime"
-                      value={overTime}
-                      onChange={(e) => {
-                        setOverTime(e.target.value);
-                      }}
                     />
                   </Col>
-                </FormGroup>
-                <FormGroup row>
-                  <Col>
-                    <Button type="submit" color="primary">
+                </Row>
+
+                <Row>
+                  <Col className="row">
+                    <Button
+                      type="submit"
+                      color="primary"
+                      className="col-3"
+                      style={{ margin: "0px 0px 20px 10px" }}
+                    >
                       Thêm
                     </Button>
                   </Col>
-                </FormGroup>
-              </Form>
+                </Row>
+              </LocalForm>
             </div>
           </div>
         </div>
       )}
-      <div className="container row menus">{Staff}</div>
+      <div className="container row menus">{Menu}</div>
     </div>
   );
 };
